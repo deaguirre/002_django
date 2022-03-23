d500653a094298326d0642bbc799412cc90e6406
Daniel Aguirre <leniadx@gmail.com>
2022-03-23 14:20:26 -0500

    App con tutotial basico

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from email.charset import BASE64
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
